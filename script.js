367706a9f168fb6dec3065dca5dabad137a06756
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-16 02:18:24 +0500

    chap 8 done

---
+++ script.js
@@ -0,0 +1,22 @@
+// chap no 8 (concatination)
+
+// in chapter 1 you learned to display a message in an alert coding in this way 
+
+// Example
+alert("thanks for everyting");
+
+const message="thank you";
+alert(message);
+
+const userName="ali";
+alert("thanks, "+userName+"!");
+
+const firstName="muhammad";
+const lastName="ali";
+const fullName=firstName + "  "+lastName;
+alert(fullName);
+
+const message="thanks";
+const userName="ali";
+const result="!";
+alert(message +" "+userName+result);
